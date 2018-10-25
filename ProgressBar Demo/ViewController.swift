932871b0dfd594407381a0c883b0c1037e3fef1b
Szabolcs Toth <tsz@purzelbaum.hu>
2018-10-26 00:23:19 +0400

    Initial Commit

---
+++ ProgressBar Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProgressBar Demo
+//
+//  Created by Szabolcs Toth on 10/26/18.
+//  Copyright © 2018 purzelbaum.hu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
